c46eceb1bbd99223ae43ae928179d8438aaaafe6
burakmert236 <burakmert236@gmail.com>
2023-07-02 09:51:00 +0300

    wheel controller

---
+++ Assets/Scripts/Wheel/WheelRotationController.cs
@@ -0,0 +1,53 @@
+using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using Wheel;
+
+public class WheelRotationController : MonoBehaviour
+{
+    public WheelRotationSettings _rotationSettings;
+    public Transform wheel; // The wheel you want to rotate
+    public Button spin_button;
+    public AudioSource _audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spin_button.onClick.AddListener(StartAndStopRotationCoroutineStarter);
+    }
+
+    private void StartAndStopRotationCoroutineStarter()
+    {
+        StartCoroutine(StartAndStopRotation());
+    }
+
+    private IEnumerator StartAndStopRotation()
+    {
+        float randomAngle = Random.Range(_rotationSettings.randomRotationMinAngle, _rotationSettings.randomRotationMaxAngle);
+
+        _audioSource.Play();
+
+        // Start the wheel rotation
+        Tween rotateTween = wheel.DORotate(new Vector3(0f, 0f, randomAngle), _rotationSettings.wheelRotationDuration, RotateMode.FastBeyond360).SetEase(Ease.InOutQuad);
+
+        // Wait for the duration before stopping the wheel
+        yield return new WaitForSeconds(_rotationSettings.wheelRotationDuration + _rotationSettings.wheelRotationDelayOffset);
+
+        _audioSource.Stop();
+
+        // Ensure the rotation tween is completed (the wheel has finished rotating)
+        // Get the current Euler angle of the wheel around the Y axis
+        float currentAngle = wheel.rotation.eulerAngles.z;
+
+        float roundedCurrentAngle = Mathf.Round(currentAngle / 45f);
+
+        Debug.Log("roundedCurrentAngle " + roundedCurrentAngle % 8);
+
+        // Round the current angle to the nearest multiple of 45 degrees
+        float roundedAngle = roundedCurrentAngle * 45f;
+
+        // Rotate the wheel to the rounded angle
+        wheel.DORotate(new Vector3(0f, 0f, roundedAngle), 1f).SetEase(Ease.OutQuad);
+    }
+}
---
+++ Assets/Scripts/Wheel/WheelRotationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+
+namespace Wheel
+{
+    [CreateAssetMenu(menuName = "Wheel/Wheel Rotation Settings")]
+    public class WheelRotationSettings : ScriptableObject
+    {
+        public float wheelRotationDuration;
+        public float wheelRotationDelayOffset;
+
+        public int randomRotationMinAngle;
+        public int randomRotationMaxAngle;
+
+    }
+}
