36cd61e52b635a09320b276cb9be2f4185511b37
burakmert236 <burakmert236@gmail.com>
2023-07-02 09:51:19 +0300

    start animation controller

---
+++ Assets/Scripts/Screen/StartAnimationController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartAnimationController : MonoBehaviour
+{
+
+    private Button ui_start_button;
+    public Animator start_screen_animator;
+
+    private void OnValidate()
+    {
+        ui_start_button = GetComponent<Button>();
+        ui_start_button.onClick.AddListener(handleStartButtonClick);
+    }
+
+    private void handleStartButtonClick()
+    {
+        start_screen_animator.SetTrigger("START");
+    }
+
+}
