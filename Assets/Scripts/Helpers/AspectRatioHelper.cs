cf115b945a06034869a34a47e2555958a181900e
burakmert236 <burakmert236@gmail.com>
2023-07-05 09:39:11 +0300

    implement aspect ratio generator script

---
+++ Assets/Scripts/Helpers/AspectRatioHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Helpers
+{
+    public class AspectRatioHelper
+    {
+        public static void aspectRatioFiltterGenerator(Image imageComponent, float scaleY)
+        {
+            AspectRatioFitter aspectRatioFitter = imageComponent.gameObject.GetComponent<AspectRatioFitter>();
+            aspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+
+            float nativeAspectRatio = imageComponent.sprite.rect.width / imageComponent.sprite.rect.height;
+            aspectRatioFitter.aspectRatio = nativeAspectRatio;
+
+            RectTransform rectTransform = imageComponent.rectTransform;
+            rectTransform.anchorMin = new Vector2(0, rectTransform.anchorMin.y);
+            rectTransform.anchorMax = new Vector2(scaleY, rectTransform.anchorMax.y);
+            rectTransform.offsetMin = new Vector2(0, rectTransform.offsetMin.y);
+            rectTransform.offsetMax = new Vector2(0, rectTransform.offsetMax.y);
+        }
+    }
+}
+
