903d29bb5d186b65b084064a9dc7c7ba52fe6938
burakmert236 <burakmert236@gmail.com>
2023-07-05 09:02:15 +0300

    spin button and back button controllers

--- Assets/Scripts/Wheel/WheelRotationController.cs
+++ Assets/Scripts/Wheel/WheelRotationController.cs
@@ -7,8 +7,13 @@ using Wheel;
 public class WheelRotationController : MonoBehaviour
 {
     public WheelRotationSettings _rotationSettings;
+
     public Transform wheel; // The wheel you want to rotate
+    public Transform parentWheelComponent;
+
     public Button spin_button;
+    public Button backButton;
+
     public AudioSource _audioSource;
 
     // Start is called before the first frame update
@@ -24,6 +29,9 @@ public class WheelRotationController : MonoBehaviour
 
     private IEnumerator StartAndStopRotation()
     {
+        backButton.interactable = false;
+        spin_button.interactable = false;
+
         float randomAngle = Random.Range(_rotationSettings.randomRotationMinAngle, _rotationSettings.randomRotationMaxAngle);
 
         _audioSource.Play();
@@ -42,12 +50,16 @@ public class WheelRotationController : MonoBehaviour
 
         float roundedCurrentAngle = Mathf.Round(currentAngle / 45f);
 
-        Debug.Log("roundedCurrentAngle " + roundedCurrentAngle % 8);
-
         // Round the current angle to the nearest multiple of 45 degrees
         float roundedAngle = roundedCurrentAngle * 45f;
 
         // Rotate the wheel to the rounded angle
         wheel.DORotate(new Vector3(0f, 0f, roundedAngle), 1f).SetEase(Ease.OutQuad);
+
+        yield return new WaitForSeconds(1f);
+
+        WheelController parentWheelComponentScript = parentWheelComponent.GetComponent<WheelController>();
+
+        parentWheelComponentScript.handleRotationEnd(roundedCurrentAngle);
     }
 }
