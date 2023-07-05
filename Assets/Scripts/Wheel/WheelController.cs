ea9071fb0d48469b8dc97bcbaed6438d49781cab
burakmert236 <burakmert236@gmail.com>
2023-07-05 09:03:08 +0300

    implement dynamic aspect ratio fitters for reward images

--- Assets/Scripts/Wheel/WheelController.cs
+++ Assets/Scripts/Wheel/WheelController.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Reward;
+using Zone;
 
 namespace Wheel
 {
@@ -12,16 +13,24 @@ namespace Wheel
 
         public WheelTypeSettings _wheelTypeSettings;
         public WheelTypeSettings _wheelIndicatorSettings;
+        public RewardTypeSettings _rewardTypeSettings;
 
         public Image wheelImage;
         public Image indicatorImage;
         public TextMeshProUGUI wheelText;
 
+        public Transform rootUITransform;
+        public Transform itemsContainer;
+        public Transform zoneIndicator;
+
+        private CollectedItemsContainerController itemsContainerScript;
+        private ZoneIndicatorController zoneIndicatorScript;
+
         [System.Serializable]
         public struct Slot
         {
             public int slotNumber;
-            public Reward.RewardTypeSettings.RewardType rewardType;
+            public RewardTypeSettings.RewardType rewardType;
             public int rewardAmount;
         }
 
@@ -34,14 +43,28 @@ namespace Wheel
 
         public Zone[] zones;
 
+        public Transform[] slotTransforms;
+
         private int currentZoneNumber;
         private WheelTypeSettings.WheelType zoneType;
+
+        public List<Slot> collectedItems;
 
         // Start is called before the first frame update
         void Start()
         {
             currentZoneNumber = 1;
+
             zoneType = WheelTypeSettings.WheelType.NORMAL;
+
+            updateUIElementsForZone(currentZoneNumber, zoneType);
+
+            collectedItems = new List<Slot>();
+
+            itemsContainerScript = itemsContainer.GetComponent<CollectedItemsContainerController>();
+            zoneIndicatorScript = zoneIndicator.GetComponent<ZoneIndicatorController>();
+
+            zoneIndicatorScript.updateZones(currentZoneNumber);
         }
 
         public void updateZoneForSuccess()
@@ -59,7 +82,8 @@ namespace Wheel
                 zoneType = WheelTypeSettings.WheelType.NORMAL;
             }
 
-            updateUIElementsForZone(zoneType);
+            updateUIElementsForZone(currentZoneNumber, zoneType);
+            wheelImage.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
 
         public void updateZoneForReset()
@@ -67,10 +91,12 @@ namespace Wheel
             currentZoneNumber = 1;
             zoneType = WheelTypeSettings.WheelType.NORMAL;
 
-            updateUIElementsForZone(zoneType);
+            updateUIElementsForZone(currentZoneNumber, zoneType);
+            wheelImage.transform.rotation = Quaternion.Euler(0, 0, 0);
+            collectedItems = new List<Slot>();
         }
 
-        private void updateUIElementsForZone (WheelTypeSettings.WheelType type)
+        private void updateUIElementsForZone (int zoneNumber, WheelTypeSettings.WheelType type)
         {
             WheelTypeSettings.WheelTypes newWheelType = _wheelTypeSettings.GetSpriteOfWheelType(type);
             WheelTypeSettings.WheelTypes newWheelIndicator = _wheelIndicatorSettings.GetSpriteOfWheelType(type);
@@ -80,6 +106,81 @@ namespace Wheel
             wheelText.text = newWheelType.text;
 
             indicatorImage.sprite = newWheelIndicator.sprite;
+
+            if(zones.Length >= zoneNumber)
+            {
+                Zone currentZone = zones[zoneNumber - 1];
+                Slot[] currentSlots = currentZone.slot;
+
+                for(int i = 0; i<currentSlots.Length; i++)
+                {
+                    Slot currentSlot = currentSlots[i];
+                    Transform currentSlotTransform = slotTransforms[i];
+                    RewardTypeSettings.RewardTypeSprites rewardTypeSprite = _rewardTypeSettings.GetSpriteOfRewardType(currentSlot.rewardType);
+
+                    WheelSlotController _wheelSlotController = currentSlotTransform.GetComponent<WheelSlotController>();
+                    _wheelSlotController.updateSlotUIElements(rewardTypeSprite.sprite, currentSlot.rewardAmount);
+                }
+            }
+        
+        }
+
+        public void handleRotationEnd(float roundedEndAngle)
+        {
+            int slotNum = ((int)roundedEndAngle) % 8;
+
+            Zone currentZone = zones[currentZoneNumber - 1];
+            Slot[] currentSlots = currentZone.slot;
+            Slot slot = currentSlots[slotNum];
+
+            RewardTypeSettings.RewardType rewardType = slot.rewardType;
+            int rewardAmount = slot.rewardAmount;
+            RewardTypeSettings.RewardTypeSprites rewardTypeSprite = _rewardTypeSettings.GetSpriteOfRewardType(rewardType);
+
+            CardPanelCreator rootUITransformScript = rootUITransform.GetComponent<CardPanelCreator>();
+            rootUITransformScript.createCardPanel(rewardTypeSprite.sprite, rewardAmount, rewardType);
+
+            if(rewardType == RewardTypeSettings.RewardType.DEATH)
+            {
+                updateZoneForReset();
+            }
+            else
+            {
+                updateZoneForSuccess();
+            }
+
+            zoneIndicatorScript.updateZones(currentZoneNumber);
+
+            addItemToCollectedList(rewardType, rewardAmount);
+        }
+
+        private void addItemToCollectedList(RewardTypeSettings.RewardType _rewardType, int _rewardAmount)
+        {
+            bool found = false;
+
+            for(int i = 0; i<collectedItems.Count; i++)
+            {
+                Slot item = collectedItems[i];
+
+                if (item.rewardType == _rewardType)
+                {
+                    item.rewardAmount += _rewardAmount;
+                    found = true;
+                    itemsContainerScript.updateITems(_rewardType, _rewardAmount);
+                    break;
+                }
+            }
+
+            if(!found)
+            {
+                Slot newSlot = new Slot();
+                newSlot.rewardType = _rewardType;
+                newSlot.rewardAmount = _rewardAmount;
+
+                itemsContainerScript.updateITems(_rewardType, _rewardAmount);
+
+                collectedItems.Add(newSlot);
+            }
         }
     }
 }
