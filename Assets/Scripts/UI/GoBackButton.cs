1af379918243aa2f19f6885c127b8b059cf55e64
burakmert236 <burakmert236@gmail.com>
2023-07-05 08:58:37 +0300

    add go back button

---
+++ Assets/Scripts/UI/GoBackButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoBackButton : MonoBehaviour
+{
+
+	private Button backButton;
+	public Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		backButton = GetComponent<Button>();
+        backButton.onClick.AddListener(goBackAnimation);
+    }
+
+	private void goBackAnimation()
+	{
+		animator.SetTrigger("BACK_TO_START");
+    }
+}
+
