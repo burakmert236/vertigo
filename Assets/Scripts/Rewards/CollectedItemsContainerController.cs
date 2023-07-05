3fea4d8192f7f10f94b39a84d12363503050b4dc
burakmert236 <burakmert236@gmail.com>
2023-07-05 09:14:49 +0300

    add collected items to a reward list on screen

---
+++ Assets/Scripts/Rewards/CollectedItemsContainerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Wheel;
+using static Reward.RewardTypeSettings;
+
+namespace Reward
+{
+	public class CollectedItemsContainerController : MonoBehaviour
+	{
+
+		public RewardTypeSettings _rewardTypeSettings;
+        public GameObject itemPrefab;
+
+		public Dictionary<RewardTypeSettings.RewardType, GameObject> itemsDict;
+
+        private void Start()
+        {
+            itemsDict = new Dictionary<RewardTypeSettings.RewardType, GameObject>();
+        }
+
+        public void updateITems(Reward.RewardTypeSettings.RewardType _rewardType, int _rewardAmount)
+		{
+
+            if(_rewardType == Reward.RewardTypeSettings.RewardType.DEATH)
+            {
+                itemsDict = new Dictionary<RewardTypeSettings.RewardType, GameObject>();
+                foreach (Transform child in transform)
+                {
+                    Destroy(child.gameObject);
+                }
+            } else if(itemsDict.ContainsKey(_rewardType))
+            {
+                GameObject existingItem = itemsDict[_rewardType];
+                CollectedItemController existingItemScript = existingItem.GetComponent<CollectedItemController>();
+                existingItemScript.updateAmountOnly(existingItemScript.currentItemAmount + _rewardAmount);
+            } else
+            {
+                GameObject newItem = Instantiate(itemPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                newItem.transform.parent = transform;
+                CollectedItemController newItemScript = newItem.GetComponent<CollectedItemController>();
+
+                RewardTypeSprites rewardTypeSprite = _rewardTypeSettings.GetSpriteOfRewardType(_rewardType);
+                newItemScript.updateItem(rewardTypeSprite.sprite, _rewardAmount);
+
+                newItem.transform.localScale = new Vector3(1f, 1f, 1f);
+
+                itemsDict.Add(_rewardType, newItem);
+            }
+        }
+	}
+}
+
