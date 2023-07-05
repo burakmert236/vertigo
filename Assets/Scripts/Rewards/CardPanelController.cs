d8d6f1138ca972c4977afb412ea997aa68fafb32
burakmert236 <burakmert236@gmail.com>
2023-07-05 09:06:29 +0300

    add aspect ratio fitter to collected item and card panel images

--- Assets/Scripts/Rewards/CardPanelController.cs
+++ Assets/Scripts/Rewards/CardPanelController.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Reward;
 
 public class CardPanelController : MonoBehaviour
 {
 
     public Image rewardImage;
+    public Image cardBackground;
     public TextMeshProUGUI rewardAmountText;
+    public TextMeshProUGUI warningText;
 
-    public void updateCardPanel (Sprite _rewardImage, int _rewardAmount)
+    private void Restart()
+    {
+        Destroy(gameObject);
+    }
+
+    public void updateCardPanel (Sprite _rewardImage, int _rewardAmount, RewardTypeSettings.RewardType _rewardType)
     {
         rewardImage.sprite = _rewardImage;
+
+        AspectRatioFitter aspectRatioFitter = rewardImage.gameObject.AddComponent<AspectRatioFitter>();
+        aspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+
+        float nativeAspectRatio = rewardImage.sprite.rect.width / rewardImage.sprite.rect.height;
+        aspectRatioFitter.aspectRatio = nativeAspectRatio;
+
+        RectTransform rectTransform = rewardImage.rectTransform;
+        rectTransform.anchorMin = new Vector2(0, rectTransform.anchorMin.y);
+        rectTransform.anchorMax = new Vector2(1f, rectTransform.anchorMax.y);
+        rectTransform.offsetMin = new Vector2(0, rectTransform.offsetMin.y);
+        rectTransform.offsetMax = new Vector2(0, rectTransform.offsetMax.y);
+
+        rewardImage.transform.localScale = new Vector3(0.7f, 0.7f, 1);
+
         rewardAmountText.text = "x" + _rewardAmount;
+
+        if(_rewardType == RewardTypeSettings.RewardType.DEATH)
+        {
+            cardBackground.color = Color.red;
+            warningText.text = "You got the bomb! You lost everthing!\nTap to start again";
+        }
     }
 }
---
+++ Assets/Scripts/Rewards/CollectedItemController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+namespace Reward
+{
+    public class CollectedItemController : MonoBehaviour
+    {
+        public Image itemRewardImage;
+        public TextMeshProUGUI rewardAmountText;
+        public int currentItemAmount;
+
+        public void updateItem(Sprite _image, int _amount)
+        {
+            itemRewardImage.sprite = _image;
+            rewardAmountText.text = "x" + _amount;
+            currentItemAmount = _amount;
+
+            AspectRatioFitter aspectRatioFitter = itemRewardImage.gameObject.AddComponent<AspectRatioFitter>();
+            aspectRatioFitter.aspectMode = AspectRatioFitter.AspectMode.FitInParent;
+
+            float nativeAspectRatio = itemRewardImage.sprite.rect.width / itemRewardImage.sprite.rect.height;
+            aspectRatioFitter.aspectRatio = nativeAspectRatio;
+
+            RectTransform rectTransform = itemRewardImage.rectTransform;
+            rectTransform.anchorMin = new Vector2(0, rectTransform.anchorMin.y);
+            rectTransform.anchorMax = new Vector2(1f, rectTransform.anchorMax.y);
+            rectTransform.offsetMin = new Vector2(0, rectTransform.offsetMin.y);
+            rectTransform.offsetMax = new Vector2(0, rectTransform.offsetMax.y);
+
+            itemRewardImage.transform.localScale = new Vector3(0.8f, 0.8f, 1f);
+        }
+
+        public void updateAmountOnly (int _amount)
+        {
+            rewardAmountText.text = "x" + _amount;
+            currentItemAmount = _amount;
+        }
+    }
+}
+
